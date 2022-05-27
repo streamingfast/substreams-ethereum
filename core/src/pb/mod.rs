e35e39d676da7d904aecffca1dbdbefd94f5eec4
Alexandre Bourget <alex@streamingfast.io>
2022-05-27 02:05:00 -0400

    Have a nicer wrapper directly in the crate.

--- core/src/pb/mod.rs
+++ core/src/pb/mod.rs
@@ -9,4 +9,7 @@ pub mod eth {
     pub mod v1 {
         pub use crate::pb::sf_ethereum_type_v1::*;
     }
+    pub mod rpc  {
+	pub use crate::pb::sf_ethereum_substreams_v1::*;
+    }
 }
--- core/src/rpc.rs
+++ core/src/rpc.rs
@@ -1,10 +1,19 @@
 use crate::externs;
 use substreams::memory;
+use substreams::proto;
+use crate::pb;
 
-pub fn eth_call(input: Vec<u8>) -> Vec<u8> {
+fn eth_call_internal(input: Vec<u8>) -> Vec<u8> {
     unsafe {
         let rpc_response_ptr = memory::alloc(8);
         externs::rpc::eth_call(input.as_ptr(), input.len() as u32, rpc_response_ptr);
         return memory::get_output_data(rpc_response_ptr);
     }
+}
+
+pub fn eth_call(input: &pb::eth::rpc::RpcCalls) -> pb::eth::rpc::RpcResponses {
+    let raw_resp: Vec<u8> = eth_call_internal(proto::encode(input).unwrap());
+    let resp: pb::eth::rpc::RpcResponses = proto::decode(&raw_resp).unwrap();
+
+    return resp;
 }
