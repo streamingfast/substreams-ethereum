2b90bdd564b8bd1c89801ff82d5f951429c5cbe8
Sebastian Siemssen <fubhy@fubhy.com>
2022-06-22 11:40:38 +0200

    fix event overloads

--- abigen/src/contract.rs
+++ abigen/src/contract.rs
@@ -21,10 +21,23 @@ pub struct Contract {
 
 impl<'a> From<&'a ethabi::Contract> for Contract {
     fn from(c: &'a ethabi::Contract) -> Self {
+        let mut events: Vec<(String, ethabi::Event)> = vec![];
+
+        for overloads in c.events.values() {
+            for (index, event) in overloads.iter().enumerate() {
+                let name = match index {
+                    0 => event.name.clone(),
+                    _ => format!("{}{}", event.name, index),
+                };
+
+                events.push((name, event.clone()));
+            }
+        }
+
         Contract {
             // constructor: c.constructor.as_ref().map(Into::into),
             // functions: c.functions().map(Into::into).collect(),
-            events: c.events().map(Into::into).collect(),
+            events: events.iter().map(Into::into).collect(),
         }
     }
 }
