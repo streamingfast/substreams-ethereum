52408a86807325266db4ad91436901ee365ef66b
Matthieu Vachon <matt@streamingfast.io>
2023-03-21 12:21:39 -0400

    Seems that waiting for crates.io update is not required anymore
    Will know for sure only on next release but I'm pretty confident, publishing now yield the message `Waiting on `substreams-ethereum` to propagate to crates.io index (ctrl-c to wait asynchronously)` with a few follow up about `Updating crates.io index` so it seems publish now properly wait to crates.io to be updated properly.

--- bin/release.sh
+++ bin/release.sh
@@ -73,14 +73,8 @@ main() {
   fi
 
   cargo publish $args --target wasm32-unknown-unknown -p substreams-ethereum-core
-
-  maybe_wait_publish
   cargo publish $args --target wasm32-unknown-unknown -p substreams-ethereum-abigen
-
-  maybe_wait_publish
   cargo publish $args --target wasm32-unknown-unknown -p substreams-ethereum-derive
-
-  maybe_wait_publish
   cargo publish $args --target wasm32-unknown-unknown -p substreams-ethereum
 }
 
@@ -109,26 +103,6 @@ verify_keybase() {
     echo "You will need to have it available ('brew install keybase' on Mac OS X) and"
     echo "configure it, just setting your Git username and a password should be enough."
     exit 1
-  fi
-}
-
-maybe_wait_publish() {
-  if [[ "$force" == "true" ]]; then
-    # We must wait a bit in-between publish lettin enough time for the crates.io registry to
-    # correctly records the newly published crate.
-    #
-    # Without this wait time, the second publish can hit `no matching package named ...` errors
-    # because the dependent crate is not "seen" by the registry yet.
-    #
-    #   Verifying substreams v0.0.8-beta (/Users/maoueh/work/sf/substreams/rust/substreams)
-    #   error: failed to verify package tarball
-    #
-    #   Caused by:
-    #     no matching package named `substreams-macro` found
-    #     location searched: registry `crates-io`
-    #     required by package `substreams v0.0.8-beta (/Users/maoueh/work/sf/substreams/target/package/substreams-0.0.8-beta)`
-    echo "Waiting 30s giving time to crates.io to updates its public registry with new version..."
-    sleep 30
   fi
 }
 
